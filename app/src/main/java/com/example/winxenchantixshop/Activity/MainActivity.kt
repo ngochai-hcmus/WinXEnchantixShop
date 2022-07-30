2612ffc43e1c0c4c7a5f5e2756a686f56a9ba928
ngochai-hcmus <hainguyen13097@gmail.com>
2022-07-30 15:18:12 +0700

    commit activity

--- app/src/main/java/com/example/winxenchantixshop/Activity/MainActivity.kt
+++ app/src/main/java/com/example/winxenchantixshop/Activity/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.winxenchantixshop.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.winxenchantixshop.Fragment.CustomerPageFragment
+import com.example.winxenchantixshop.Fragment.HomePageFragment
 import com.example.winxenchantixshop.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var currentFragment : Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //BottomNavigationView + Fragment
+        supportFragmentManager.beginTransaction().replace(R.id.frame_layout,HomePageFragment()).commit()
+        val bottomNav : BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home ->  {currentFragment = HomePageFragment()}
+                R.id.user -> {currentFragment = CustomerPageFragment()}
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.frame_layout,currentFragment).commit()
+            true
+        }
+
     }
 }
