019b0586b248369edcea3a5c6cf0bc9ce699c4eb
thai <thai4362@gmail.com>
2022-08-01 16:17:09 +0700

    test commit login activity

--- app/src/main/java/com/example/winxenchantixshop/Activity/LoginRegister/LoginActivity.kt
+++ app/src/main/java/com/example/winxenchantixshop/Activity/LoginRegister/LoginActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.content.Intent
 import com.example.winxenchantixshop.databinding.ActivityLoginBinding
 import com.example.winxenchantixshop.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding : ActivityLoginBinding
+    private lateinit var database : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
