728c10b00a5ba10222606a76056d3b4bc0290e3a
thai <thai4362@gmail.com>
2022-08-01 16:09:42 +0700

    test commit

--- app/src/main/java/com/example/winxenchantixshop/Activity/LoginRegister/RegisterActivity.kt
+++ app/src/main/java/com/example/winxenchantixshop/Activity/LoginRegister/RegisterActivity.kt
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.winxenchantixshop.databinding.ActivityMainBinding
 import com.example.winxenchantixshop.databinding.ActivityRegisterBinding
+import com.google.firebase.database.DatabaseReference
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var database : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
