34a77bfc47e5aad19b28f9bb9b27b441d50df841
ngochai-hcmus <hainguyen13097@gmail.com>
2022-08-04 16:07:09 +0700

    commit activity

--- app/src/main/java/com/example/winxenchantixshop/Activity/Product/ProductInformationActivity.kt
+++ app/src/main/java/com/example/winxenchantixshop/Activity/Product/ProductInformationActivity.kt
@@ -2,6 +2,8 @@ package com.example.winxenchantixshop.Activity.Product
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import com.example.winxenchantixshop.R
 import com.squareup.picasso.Picasso
@@ -10,6 +12,9 @@ import com.example.winxenchantixshop.databinding.ActivityVerifyOtpBinding
 
 class ProductInformationActivity : AppCompatActivity() {
     private lateinit var binding : ActivityProductInformationBinding
+    private var quantity = 0
+    private var imageUrl = ""
+    private var productName =""
 
     override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
@@ -21,8 +26,8 @@ class ProductInformationActivity : AppCompatActivity() {
 
         val intent = intent
 
-        val imageUrl = intent.getStringExtra("imageUrl")
-        val productName = intent.getStringExtra("productName")
+        imageUrl = intent.getStringExtra("imageUrl").toString()
+        productName = intent.getStringExtra("productName").toString()
         val category = intent.getStringExtra("category")
         val price = intent.getStringExtra("price")
         val amount = intent.getStringExtra("amount")
@@ -35,6 +40,34 @@ class ProductInformationActivity : AppCompatActivity() {
         binding.productname.text = productName
         binding.productprice.text = price
         binding.productdes.text = description
+        binding.btnaddcart.setOnClickListener{
+
+        }
 
     }
+
+    fun checkIsInCart() {
+
+    }
+
+    fun addToCart() {
+
+    }
+
+    fun minusQuantity(view: View) {
+        quantity--
+        if(quantity < 0){
+            quantity = 0
+        }
+        val textview = findViewById(R.id.number) as TextView
+        textview.text = "$quantity"
+
+    }
+    fun plusQuantity(view: View) {
+        quantity++
+        val textview = findViewById(R.id.number) as TextView
+        textview.text = "$quantity"
+    }
+
+
 }
