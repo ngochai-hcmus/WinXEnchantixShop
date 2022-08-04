4219effb8fcdbefa2e65cafbdbb6fde2bbddd070
thai <thai4362@gmail.com>
2022-08-04 21:38:17 +0700

    connect UI and Activity

--- app/src/main/java/com/example/winxenchantixshop/Activity/AdminSeller/OrderHistoryActivity.kt
+++ app/src/main/java/com/example/winxenchantixshop/Activity/AdminSeller/OrderHistoryActivity.kt
@@ -2,13 +2,13 @@ package com.example.winxenchantixshop.Activity.AdminSeller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.winxenchantixshop.databinding.ActivityMyProductBinding
+import com.example.winxenchantixshop.databinding.ActivityOrderHistoryBinding
 
-class MyProductActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMyProductBinding
+class OrderHistoryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOrderHistoryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMyProductBinding.inflate(layoutInflater)
+        binding = ActivityOrderHistoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
