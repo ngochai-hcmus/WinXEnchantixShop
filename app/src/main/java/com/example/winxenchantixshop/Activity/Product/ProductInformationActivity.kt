c29d01b3a583cb4f08f21debb8a5cbe520afb0dc
huyhoangg <huyhoang1472@gmail.com>
2022-08-15 11:19:56 +0700

    commit fix buy no item

--- app/src/main/java/com/example/winxenchantixshop/Activity/Product/ProductInformationActivity.kt
+++ app/src/main/java/com/example/winxenchantixshop/Activity/Product/ProductInformationActivity.kt
@@ -56,12 +56,18 @@ class ProductInformationActivity : AppCompatActivity() {
         }
 
         binding.btnbuy.setOnClickListener {
-            val intent = Intent(this, OrderActivity::class.java)
-            val product = Product(imageUrl, productName, category, price, binding.number.text.toString(), description)
-            listProduct.clear()
-            listProduct.add(product)
-            intent.putExtra("listProduct", listProduct)
-            startActivity(intent)
+            if (binding.number.text.toString() != "0") {
+                val intent = Intent(this, OrderActivity::class.java)
+                val product = Product(imageUrl, productName, category, price, binding.number.text.toString(), description)
+                listProduct.clear()
+                listProduct.add(product)
+                intent.putExtra("listProduct", listProduct)
+                startActivity(intent)
+            }
+            else {
+                Toast.makeText(this, "Please choose quantity first", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
         binding.btnaddcart.setOnClickListener {
