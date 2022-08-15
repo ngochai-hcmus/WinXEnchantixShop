da49551252efd10e181ae6408e84b43342deefec
ngochai-hcmus <hainguyen13097@gmail.com>
2022-08-15 22:41:06 +0700

    commit activity

--- app/src/main/java/com/example/winxenchantixshop/Activity/AdminSeller/ListSellerActivity.kt
+++ app/src/main/java/com/example/winxenchantixshop/Activity/AdminSeller/ListSellerActivity.kt
@@ -2,47 +2,82 @@ package com.example.winxenchantixshop.Activity.AdminSeller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.winxenchantixshop.Adapter.AccountAdapter
-import com.example.winxenchantixshop.R
+import com.example.winxenchantixshop.Activity.Util.SwipeToDeleteCallback
+import com.example.winxenchantixshop.Adapter.UserInforAdapter
 import com.google.firebase.database.*
-import com.example.winxenchantixshop.DTO.Account
+import com.example.winxenchantixshop.DTO.UserInfor
+import com.example.winxenchantixshop.databinding.ActivityListSellerBinding
 
 class ListSellerActivity : AppCompatActivity() {
 
-    private lateinit var dbref: DatabaseReference
-    private lateinit var AccountRecyclerview: RecyclerView
-    private lateinit var AccountArrayList: ArrayList<Account>
+    private lateinit var binding: ActivityListSellerBinding
+    private lateinit var db_ref: DatabaseReference
+    private lateinit var UserRecyclerview: RecyclerView
+    private lateinit var UserAdapter: UserInforAdapter
+    private lateinit var UserList: ArrayList<UserInfor>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_seller)
-        AccountRecyclerview = findViewById(R.id.recycle_list_seller)
-        AccountRecyclerview.layoutManager = LinearLayoutManager(this)
-        AccountRecyclerview.setHasFixedSize(true)
+        binding = ActivityListSellerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        UserRecyclerview = binding.recycleListSeller
+        UserRecyclerview.layoutManager = LinearLayoutManager(this)
+        UserRecyclerview.setHasFixedSize(true)
 
-        AccountArrayList = arrayListOf<Account>()
-        getAccountData()
+        UserList = arrayListOf<UserInfor>()
+        UserAdapter = UserInforAdapter(UserList)
+        getUserData()
+
+        val swipeToDeleteCallback = object : SwipeToDeleteCallback() {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val pos : Int = viewHolder.adapterPosition
+                val userToDelete : UserInfor = UserList.get(pos)
+                val name = userToDelete.name.toString()
+                deleteUser(name)
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
+        itemTouchHelper.attachToRecyclerView(UserRecyclerview)
 
     }
 
-    private fun getAccountData() {
-        dbref = FirebaseDatabase.getInstance().getReference("Account")
-        val okQuery = dbref.orderByChild("type").equalTo("seller")
+    private fun deleteUser(name: String) {
+        UserList = arrayListOf<UserInfor>()
+        db_ref = FirebaseDatabase.getInstance().getReference("UserInfor")
+
+        db_ref.child(name).removeValue().addOnSuccessListener {
+            //Toast.makeText(applicationContext, "Deleted 1 item", Toast.LENGTH_LONG).show()
+        }.addOnFailureListener {
+            //Toast.makeText(applicationContext, "Failed to delete, try again", Toast.LENGTH_LONG).show()
+        }
+
+        db_ref = FirebaseDatabase.getInstance().getReference("Account")
+        db_ref.child(name).removeValue().addOnSuccessListener {
+            Toast.makeText(applicationContext, "Deleted 1 item", Toast.LENGTH_LONG).show()
+        }.addOnFailureListener {
+            Toast.makeText(applicationContext, "Failed to delete, try again", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun getUserData() {
+        UserList = arrayListOf<UserInfor>()
+        db_ref = FirebaseDatabase.getInstance().getReference("UserInfor")
+        val okQuery = db_ref.orderByChild("type").equalTo("seller")
         okQuery.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
+                UserList.clear()
                 if (snapshot.exists()) {
-                    for (accountSnapshot in snapshot.children) {
-                        val account = accountSnapshot.getValue(Account::class.java)
-
-                        AccountArrayList.add(account!!)
-
+                    for (userSnapshot in snapshot.children) {
+                        val user = userSnapshot.getValue(UserInfor::class.java)
+                        UserList.add(user!!)
 
                     }
-
-                    AccountRecyclerview.adapter = AccountAdapter(AccountArrayList)
+                    UserRecyclerview.adapter = UserInforAdapter(UserList)
                 }
             }
 
