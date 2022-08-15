ae86095f7e971b8878e22d7e6b10d60cab2def74
huyhoangg <huyhoang1472@gmail.com>
2022-08-15 11:26:31 +0700

    fix order of no item with 30k ship

--- app/src/main/java/com/example/winxenchantixshop/Activity/Product/CartActivity.kt
+++ app/src/main/java/com/example/winxenchantixshop/Activity/Product/CartActivity.kt
@@ -54,9 +54,14 @@ class CartActivity : AppCompatActivity() {
         itemTouchHelper.attachToRecyclerView(itemRecyclerView)
 
         binding.buttonOrder.setOnClickListener {
-            intent = Intent(this, OrderActivity::class.java)
-            intent.putExtra("listProduct", listProduct)
-            startActivity(intent)
+            if (listProduct.isNotEmpty()) {
+                intent = Intent(this, OrderActivity::class.java)
+                intent.putExtra("listProduct", listProduct)
+                startActivity(intent)
+            }
+            else {
+                Toast.makeText(this, "Please select at least 1 item", Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
